60b9c45dec9865e83c19e53b2cda7c75c6a8452c
Mitchell Rosen <mitchellwrosen@gmail.com>
2024-04-22 11:30:23 -0400

    work

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@ import Data.Bits (unsafeShiftL)
 import Data.Foldable (fold)
 import Data.Foldable qualified as Foldable
 import Data.Function ((&))
+import Data.List qualified as List
 import Data.Maybe (fromJust)
 import Data.Sequence (Seq)
 import Data.Sequence qualified as Seq
@@ -23,14 +24,15 @@ import Termbox.Tea
 import Text.Printf (printf)
 import Utils.Containers.Internal.BitUtil (highestBitMask)
 import Witherable (catMaybes)
+import Prelude hiding (last)
 
 main :: IO ()
 main = do
   pongQueue <- newTQueueIO
-
+  beginning <- getMonotonicTime
   Ki.scoped \scope -> do
     Ki.fork_ scope (runPingThread pongQueue)
-    tuiMain pongQueue
+    tuiMain pongQueue beginning
 
 runPingThread :: TQueue Pong -> IO Void
 runPingThread pongQueue =
@@ -45,84 +47,40 @@ runPingThread pongQueue =
     config =
       (shell "ping -i 0.25 -Q 8.8.8.8") {std_err = NoStream, std_out = CreatePipe}
 
-tuiMain :: TQueue Pong -> IO ()
-tuiMain pongQueue = do
-  let initialize :: Size -> S
+tuiMain :: TQueue Pong -> Double -> IO ()
+tuiMain pongQueue beginning = do
+  let initialize :: Size -> State
       initialize size =
-        S
+        State
           { done = False,
             pongs = Seq.empty,
-            size
+            size,
+            smoothLevel = SmoothLevel0,
+            timestamp = beginning
           }
 
   let pollEvent :: Maybe (IO Pong)
       pollEvent =
         Just (atomically (readTQueue pongQueue))
 
-  let handleEvent :: S -> Event Pong -> IO S
+  let handleEvent :: State -> Event Pong -> IO State
       handleEvent state event = do
         pure case event of
           EventKey key ->
             case key of
               KeyChar 'q' -> state {done = True}
+              KeyChar 's' -> state {smoothLevel = cycleSmoothLevel state.smoothLevel}
               KeyEsc -> state {done = True}
               _ -> state
           EventResize size -> state {size}
           EventMouse _ -> state
-          EventUser pong -> state {pongs = insertPong pong state.pongs}
+          EventUser pong ->
+            state
+              { pongs = insertPong pong state.pongs,
+                timestamp = pong.timestamp
+              }
 
-  let render :: S -> Scene
-      render state =
-        let barsPos = Pos {row = 2, col = 5}
-            barsSize =
-              Size
-                { width = state.size.width - barsPos.col,
-                  height = state.size.height - (2 * barsPos.row)
-                }
-
-            -- the second +1 is optional, just favors lower bars
-            dottedRow = (barsPos.row + barsSize.height + 2) `div` 2
-
-            buckets = bucketThePongs (.icmp) barsSize.width state.pongs
-            buckets1 = summarizeBucket (.milliseconds) <$> buckets
-
-            slowest = summarizeBuckets (\(_, _, v) -> v) buckets1
-            -- slowest up to next power of 2, e.g. 117 -> 128
-            slowest1 =
-              let m = highestBitMask (round @Double @Word slowest)
-               in realToFrac @Word @Double (unsafeShiftL m 1)
-            vh = max 1 . round @Double @Int . (* i2d barsSize.height) . (/ slowest1)
-
-            rect :: Pos -> Size -> Image -> Image
-            rect pos size cell =
-              [0 .. size.height - 1] & foldMap \row ->
-                [0 .. size.width - 1] & foldMap \col ->
-                  cell & at (pos <> Pos {row, col})
-         in (image . fold)
-              [ -- Draw the axes
-                foldMap (\col -> char '┄' & at Pos {row = dottedRow, col}) [0 .. state.size.width - 1],
-                let col = barsPos.col - 1
-                 in foldMap (\row -> char '│' & at Pos {row, col}) [barsPos.row .. barsPos.row + barsSize.height - 1],
-                foldMap (\col -> char '─' & at Pos {row = state.size.height - 2, col}) [0 .. state.size.width - 1],
-                (char '┼' & at Pos {row = barsPos.row + barsSize.height, col = barsPos.col - 1}),
-                (char '┼' & at Pos {row = dottedRow, col = barsPos.col - 1}),
-                printf "% 4.0f┼" slowest1 & zip [0..] & foldMap \(col, c) ->
-                  char c & at Pos {row = barsPos.row, col},
-                -- Draw the buckets
-                ( buckets1 & Foldable.toList & zip [0 ..] & foldMap \(i, (_, x, v)) ->
-                    let row = barsPos.row + barsSize.height - height
-                        col = barsPos.col + i * width
-                        width = 1
-                        height = if x == 0 then vh v else 0
-                     in rect Pos {row, col} Size {width, height} (char ' ' & bg (if x == 0 then green else yellow))
-                          <> ( if row <= dottedRow
-                                 then char '┄' & fg (gray 0) & bg green & at Pos {row = dottedRow, col}
-                                 else mempty
-                             )
-                )
-              ]
-
-  let finished :: S -> Bool
+  let finished :: State -> Bool
       finished state =
         state.done
 
@@ -132,7 +90,7 @@ tuiMain pongQueue = do
         { initialize,
           pollEvent,
           handleEvent,
-          render,
+          render = renderState beginning,
           finished
         }
 
@@ -140,12 +98,121 @@ tuiMain pongQueue = do
     Left _ -> exitFailure
     Right _ -> pure ()
 
-data S = S
+data State = State
   { done :: !Bool, -- Has the user requested we quit the program?
     pongs :: !(Seq Pong), -- Every pong we've received, ordered by sequence number
-    size :: {-# UNPACK #-} !Size -- The size of the terminal
+    size :: {-# UNPACK #-} !Size, -- The size of the terminal
+    smoothLevel :: !SmoothLevel, -- How much to smooth?
+    timestamp :: {-# UNPACK #-} !Double -- The current monotonic time
   }
   deriving stock (Show)
+
+data SmoothLevel
+  = SmoothLevel0
+  | SmoothLevel1
+  | SmoothLevel2
+  deriving stock (Show)
+
+cycleSmoothLevel :: SmoothLevel -> SmoothLevel
+cycleSmoothLevel = \case
+  SmoothLevel0 -> SmoothLevel1
+  SmoothLevel1 -> SmoothLevel2
+  SmoothLevel2 -> SmoothLevel0
+
+renderState :: Double -> State -> Scene
+renderState beginning state =
+  let barsPos = Pos {row = 2, col = 5}
+      barsSize =
+        Size
+          { width = state.size.width - 2,
+            height = state.size.height - 5
+          }
+
+      -- the first +1 is for correctness, and the second is is optional, it just favors lower bars
+      oneHalfRow = (barsPos.row + barsSize.height + 2) `div` 2
+      oneQuarterRow = (barsPos.row + barsSize.height + 2) * 3 `div` 4
+      oneEighthRow = (barsPos.row + barsSize.height + 2) * 7 `div` 8
+      dottedRows = [oneHalfRow, oneQuarterRow, oneEighthRow]
+
+      -- Bucket all the pongs per the number of bars we can show
+      buckets = bucketThePongs (.icmp) barsSize.width state.pongs
+
+      -- Summarize each bucket
+      summaries0 = summarizeBucket (.milliseconds) <$> buckets
+
+      -- Smooth each summary a bit by its neighbors (so the UI is less jumpy as values move between buckets)
+      summaries =
+        let smooth =
+              case state.smoothLevel of
+                SmoothLevel0 -> id
+                SmoothLevel1 -> smoothBuckets3 (.averagePong) (\averagePong summary -> summary {averagePong})
+                SmoothLevel2 -> smoothBuckets5 (.averagePong) (\averagePong summary -> summary {averagePong})
+         in smooth summaries0
+
+      slowest = roundUp2 (summarizeBuckets (.averagePong) summaries)
+      vh = (`divMod` 8) . max 1 . round @Double @Int . (* 8) . (* i2d barsSize.height) . (/ w2d slowest)
+
+      rect :: Pos -> Size -> Image -> Image
+      rect pos size cell =
+        [0 .. size.height - 1] & foldMap \row ->
+          [0 .. size.width - 1] & foldMap \col ->
+            cell & at (pos <> Pos {row, col})
+   in (image . fold)
+        [ -- Draw the dotted rows all the way across
+          dottedRows & foldMap \row ->
+            [barsPos.col .. barsPos.col + barsSize.width - 1] & foldMap \col ->
+              char '┄' & at Pos {row, col},
+          -- Draw the Y axis
+          let col = barsPos.col - 1
+           in fold
+                [ [barsPos.row .. barsPos.row + barsSize.height - 1] & foldMap \row -> char '│' & at Pos {row, col},
+                  dottedRows & foldMap \row -> char '┼' & at Pos {row, col}
+                ],
+          -- Draw the X axis
+          let row = barsPos.row + barsSize.height
+           in fold
+                [ [barsPos.col .. barsPos.col + barsSize.width - 1] & foldMap \col -> char '─' & at Pos {row, col},
+                  printf "%.0fs" (state.timestamp - beginning) & zip [0 ..] & foldMap \(i, c) ->
+                    let col = barsPos.col + i in char c & at Pos {row, col}
+                ],
+          -- Draw the corner
+          char '└' & at Pos {row = barsPos.row + barsSize.height, col = barsPos.col - 1},
+          -- Draw the top-left label
+          printf "%4d┼" slowest & zip [0 ..] & foldMap \(col, c) ->
+            char c & at Pos {row = barsPos.row, col},
+          -- Draw the buckets
+          summaries & Foldable.toList & zip [0 ..] & foldMap \(i, summary) ->
+            let pos =
+                  Pos
+                    { row = barsPos.row + barsSize.height - size.height,
+                      col = barsPos.col + i * size.width
+                    }
+                (size, cap) =
+                  let (height, cap0) = if summary.numTimedOutPongs == 0 then vh summary.averagePong else (0, 0)
+                   in (Size {width = 1, height}, cap0)
+                capi =
+                  case cap of
+                    1 -> char '▁'
+                    2 -> char '▂'
+                    3 -> char '▃'
+                    4 -> char '▄'
+                    5 -> char '▅'
+                    6 -> char '▆'
+                    7 -> char '▇'
+                    _ -> mempty
+             in fold
+                  [ rect pos size (char ' ' & bg green),
+                    capi & fg green & at (pos & posUp 1),
+                    dottedRows & foldMap \row ->
+                      if pos.row <= row
+                        then let col = pos.col in char '┄' & fg (gray 0) & bg green & at Pos {row, col}
+                        else mempty
+                  ],
+          -- Draw some temporary boundaries just to see stuff
+          [0, 1, state.size.height - 2, state.size.height - 1] & foldMap \row ->
+            [0 .. state.size.width - 1] & foldMap \col ->
+              char ' ' & bg (gray 6) & at Pos {row, col}
+        ]
 
 data Pong = Pong
   { timestamp :: {-# UNPACK #-} !Double, -- monotonic time this pong was received
@@ -168,36 +235,171 @@ parsePong timestamp line
       Just Pong {timestamp, icmp, milliseconds = 0}
   | otherwise = Nothing
 
--- Insert a pong at the end, maintaining sorted icmp sequence number order
+-- Insert a pong at the end, maintaining sorted icmp sequence number order. We may get duplicate icmp pongs, e.g.
+-- packet 68 timed out, then we got packet 68 back. That's kinda weird, but the ping executable just works like that?
+-- So, let's just replace whatever's there, if anything.
 insertPong :: Pong -> Seq Pong -> Seq Pong
 insertPong pong = \case
   Seq.Empty -> Seq.singleton pong
-  xs@(ys Seq.:|> z)
-    | pong.icmp > z.icmp -> xs Seq.|> pong
-    | otherwise -> insertPong pong ys Seq.|> z
+  xs@(ys Seq.:|> z) ->
+    case compare pong.icmp z.icmp of
+      LT -> insertPong pong ys Seq.|> z
+      EQ -> ys Seq.|> pong -- overwrite existing pong with same sequence number
+      GT -> xs Seq.|> pong
 
 whenJust :: (Applicative m) => Maybe a -> (a -> m ()) -> m ()
 whenJust x f =
   maybe (pure ()) f x
 
+data BucketSummary = BucketSummary
+  { numPongs :: {-# UNPACK #-} !Int, -- total number of pongs, including timed-out ones
+    numTimedOutPongs :: {-# UNPACK #-} !Int, -- total number of timed-out pongs
+    averagePong :: {-# UNPACK #-} !Double -- average value of non-timed-out pongs
+  }
+
 -- Summarize a bucket as the total number of pongs, the amount of those that timed out (value 0), and the average of the
 -- ones that didn't time out (value more than 0).
-summarizeBucket :: (a -> Double) -> Seq a -> (Int, Int, Double)
+summarizeBucket :: (a -> Double) -> Seq a -> BucketSummary
 summarizeBucket getValue =
-  Foldl.fold $
-    (\c x s -> (c, x, divide s (c - x)))
-      <$> Foldl.length
-      <*> Foldl.Fold (\acc x -> if getValue x == 0 then acc + 1 else acc) 0 id
-      <*> Foldl.Fold (\acc x -> acc + getValue x) 0 id
+  Foldl.fold (summarizeBucketFold getValue)
+
+summarizeBucketFold :: (a -> Double) -> Foldl.Fold a BucketSummary
+summarizeBucketFold getValue =
+  ( \numPongs numTimedOutPongs pongTotal ->
+      BucketSummary
+        { numPongs,
+          numTimedOutPongs,
+          averagePong = pongTotal `divide` (numPongs - numTimedOutPongs)
+        }
+  )
+    <$> Foldl.length
+    <*> Foldl.Fold (\acc x -> if getValue x == 0 then acc + 1 else acc) 0 id
+    <*> Foldl.Fold (\acc x -> acc + getValue x) 0 id
   where
     divide :: Double -> Int -> Double
     divide _ 0 = 0
     divide s x = s / i2d x
 
+smoothBuckets3 :: forall a. (a -> Double) -> (Double -> a -> a) -> Seq a -> Seq a
+smoothBuckets3 getValue setValue =
+  smooth3
+    (\() me () -> me)
+    (\() me z -> on2 z me)
+    (\x me () -> on2 x me)
+    (\x me z -> setValue ((getValue x + getValue me + getValue z) / 3) me)
+  where
+    on2 x me = setValue ((getValue x + getValue me) / 2) me
+
+smoothBuckets5 :: forall a. (a -> Double) -> (Double -> a -> a) -> Seq a -> Seq a
+smoothBuckets5 getValue setValue =
+  smooth5
+    (\() () me () () -> me)
+    (\() () me a () -> on2 a me)
+    (\() a me () () -> on2 a me)
+    (\() () me a b -> on3 a b me)
+    (\() a me b () -> on3 a b me)
+    (\a b me () () -> on3 a b me)
+    (\() a me b c -> on4 a b c me)
+    (\a b me c () -> on4 a b c me)
+    (\a b me c d -> on5 a b c d me)
+  where
+    on2 a me = setValue (summy [a, me] / 2) me
+    on3 a b me = setValue (summy [a, b, me] / 3) me
+    on4 a b c me = setValue (summy [a, b, c, me] / 4) me
+    on5 a b c d me = setValue (summy [a, b, c, d, me] / 5) me
+
+    summy :: [a] -> Double
+    summy = List.foldl' (\acc x -> acc + getValue x) 0
+
+-- Smooth a sequence by applying a function to an element and its surrounding values.
+--
+-- >>> smooth3 (\() y () -> y) (\() y z -> y) (\x y () -> y) (\x y z -> x + y + z) (Seq.fromList [1,2,3,4,5])
+-- fromList [1,6,9,12,5]
+--
+-- >>> smooth3 (\() y () -> False) (\() y z -> True) (\x y () -> True) (\x y z -> True) (Seq.fromList [1])
+-- fromList [False]
+smooth3 ::
+  (() -> a -> () -> b) ->
+  (() -> a -> a -> b) ->
+  (a -> a -> () -> b) ->
+  (a -> a -> a -> b) ->
+  Seq a ->
+  Seq b
+smooth3 f1 f2 f3 f4 = \case
+  Seq0 -> Seq.empty
+  Seq1 x -> Seq.singleton (f1 () x ())
+  Seq2p x y ys -> go (Seq.singleton (f2 () x y)) x y ys
+  where
+    go acc x y = \case
+      Seq.Empty -> acc Seq.|> f3 x y ()
+      z Seq.:<| zs -> go (acc Seq.|> f4 x y z) y z zs
+
+-- Smooth a sequence by applying a function to an element and its surrounding values.
+smooth5 ::
+  forall a b.
+  (() -> () -> a -> () -> () -> b) ->
+  (() -> () -> a -> a -> () -> b) ->
+  (() -> a -> a -> () -> () -> b) ->
+  (() -> () -> a -> a -> a -> b) ->
+  (() -> a -> a -> a -> () -> b) ->
+  (a -> a -> a -> () -> () -> b) ->
+  (() -> a -> a -> a -> a -> b) ->
+  (a -> a -> a -> a -> () -> b) ->
+  (a -> a -> a -> a -> a -> b) ->
+  Seq a ->
+  Seq b
+smooth5 f1 f2 f3 f4 f5 f6 f7 f8 f9 = \case
+  Seq0 -> Seq.empty
+  Seq1 a -> Seq.singleton (f1 () () a () ())
+  Seq2 a b -> f2 () () a b () Seq.<| Seq.singleton (f3 () a b () ())
+  Seq3 a b c -> f4 () () a b c Seq.<| f5 () a b c () Seq.<| Seq.singleton (f6 a b c () ())
+  Seq4p a b c d ds -> go (f4 () () a b c Seq.<| Seq.singleton (f7 () a b c d)) a b c d ds
+  where
+    go :: Seq b -> a -> a -> a -> a -> Seq a -> Seq b
+    go acc a b c d = \case
+      Seq.Empty -> acc Seq.|> f8 a b c d ()
+      e Seq.:<| es -> go (acc Seq.|> f9 a b c d e) b c d e es
+
+-- Exactly 0 elements
+pattern Seq0 :: Seq a
+pattern Seq0 <- Seq.Empty
+
+-- Exactly 1 element
+pattern Seq1 :: a -> Seq a
+pattern Seq1 a <- a Seq.:<| Seq0
+
+-- Exactly 2 elements
+pattern Seq2 :: a -> a -> Seq a
+pattern Seq2 a b <- Seq2p a b Seq0
+
+-- 2+ elements
+pattern Seq2p :: a -> a -> Seq a -> Seq a
+pattern Seq2p a b bs <- a Seq.:<| b Seq.:<| bs
+
+-- Exactly 3 elements
+pattern Seq3 :: a -> a -> a -> Seq a
+pattern Seq3 a b c <- Seq3p a b c Seq0
+
+-- 3+ elements
+pattern Seq3p :: a -> a -> a -> Seq a -> Seq a
+pattern Seq3p a b c cs <- a Seq.:<| Seq2p b c cs
+
+-- 4+ elements
+pattern Seq4p :: a -> a -> a -> a -> Seq a -> Seq a
+pattern Seq4p a b c d ds <- a Seq.:<| Seq3p b c d ds
+
+{-# COMPLETE Seq0, Seq1, Seq2p #-}
+
+{-# COMPLETE Seq0, Seq1, Seq2, Seq3, Seq4p #-}
+
 -- Summarize a collection of buckets as the maximum value.
 summarizeBuckets :: (a -> Double) -> Seq a -> Double
 summarizeBuckets getValue =
-  Foldl.fold (Foldl.Fold (\acc x -> max acc (getValue x)) 0 id)
+  Foldl.fold (summarizeBucketsFold getValue)
+
+summarizeBucketsFold :: (a -> Double) -> Foldl.Fold a Double
+summarizeBucketsFold getValue =
+  Foldl.Fold (\acc x -> max acc (getValue x)) 0 id
 
 -- Bucket an array of sequence-ordered things (possibly with holes):
 --
@@ -220,10 +422,12 @@ bucketThePongs getSeqnum buckets pongs
   -- below here we know that buckets >= 2, and length pongs >= 3
   | otherwise =
       case pongs of
-        (getSeqnum -> minSeqnum) Seq.:<| (_ Seq.:|> (getSeqnum -> maxSeqnum)) ->
-          let bucketSize = realToFrac @Int @Rational (maxSeqnum - minSeqnum + 1) / realToFrac @Int @Rational buckets
+        first Seq.:<| (_ Seq.:|> last) ->
+          let bucketSize = rat (getSeqnum last - getSeqnum first + 1) / rat buckets
            in catMaybes <$> chunksOf bucketSize (fillHoles getSeqnum pongs)
         _ -> undefined
+  where
+    rat = realToFrac @Int @Rational
 
 -- >>> fillHoles id (Seq.fromList [4,7,8,10])
 -- fromList [Just 4,Nothing,Nothing,Just 7,Just 8,Nothing,Just 10]
@@ -259,3 +463,21 @@ chunksOf n
 
 i2d :: Int -> Double
 i2d = realToFrac
+
+w2d :: Word -> Double
+w2d = realToFrac
+
+-- Round up to the next power of 2.
+--
+-- >>> roundUp2 117
+-- 128
+--
+-- >>> roundUp2 128
+-- 128
+roundUp2 :: Double -> Word
+roundUp2 n
+  | w1 == w2 = w2
+  | otherwise = unsafeShiftL w2 1
+  where
+    w1 = round @Double @Word n
+    w2 = highestBitMask w1
